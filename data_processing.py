a6cb5e9410ad41312769f67f820b6174621be766
CharlestonA <charlestonambrose@gmail.com>
2026-04-07 19:31:44 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print("adding all the data processing code here")
+
+a = 1
+b = 2   
+
+def add(a, b):
+    c= a+b
+    return a
+
+print ("added a new  function to add two numbers which is not correct")
